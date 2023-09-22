5455301b25147b8642f168ed888b222dce494e59
FeliO2690 <felipe.eut262003@gmail.com>
2023-09-22 18:28:03 -0500

    primer cambio

---
+++ src/com/examen/concurrencia/Reloj.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.examen.concurrencia;
+
+import java.awt.Font;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author felip
+ */
+public class Reloj extends JFrame {
+
+    private JLabel labelHora;
+
+    public Reloj() {
+
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        labelHora = new JLabel();
+        labelHora.setHorizontalAlignment(SwingConstants.CENTER);
+        labelHora.setFont(new Font("Century", Font.PLAIN, 25));
+
+        getContentPane().add(labelHora);
+
+        Thread hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    actualizarHora();
+
+                    try {
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException e) {
+                        System.out.println("Hilo interrumpido");
+                    }
+                }
+            }
+        }) {
+
+        };
+        hilo.start();
+    }
+
+    private void actualizarHora() {
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String clock = format.format(new Date());
+        labelHora.setText(clock);
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Reloj().setVisible(true);
+            }
+        });
+
+    }
+
+}
